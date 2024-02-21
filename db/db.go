7701899f0dddf118e67083556d51f556a262beec
Nathan Faria <nathanfaria607@gmail.com>
2024-02-21 16:06:38 -0300

    refactor: put comments in the db file

--- db/db.go
+++ db/db.go
@@ -9,6 +9,12 @@ import (
 var DB *sql.DB
 
 func InitDB() {
+
+	//Sqllite is a single file to handle sql
+
+	//For use of real databases, instead of 'api.db' put the connection string to your database
+	//And and the right driver instead of 'github.com/mattn/go-sqlite3'
+
 	DB, err := sql.Open("sqlite3", "api.db")
 
 	if err != nil {
