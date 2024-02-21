6313fe27f7c27778429c5179acfc716821d08d43
Nathan Faria <nathanfaria607@gmail.com>
2024-02-21 17:23:37 -0300

    feat: implement save data in db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/ichbinnichts/events-api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,8 +17,28 @@ type Event struct {
 
 var events = []Event{}
 
-func (e *Event) Save() {
-	events = append(events, *e)
+func (e *Event) Save() error {
+	query := `INSERT INTO events (name, description, location, dateTime, user_id)
+		VALUES(?, ?, ?, ?, ?)`
+
+	preperaStatement, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer preperaStatement.Close()
+
+	result, err := preperaStatement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
