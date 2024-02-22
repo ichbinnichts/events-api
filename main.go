f2783ac58316237516d4495aafbe3f69dd7ef66c
Nathan Faria <nathanfaria607@gmail.com>
2024-02-22 07:52:56 -0300

    feat: add get single event route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ichbinnichts/events-api/db"
@@ -18,6 +19,8 @@ func main() {
 
 	server.GET("/events", getEvents)
 
+	server.GET("/events/:id", getEventById)
+
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
@@ -25,6 +28,24 @@ func main() {
 
 func getHome(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "Everythings fine"})
+}
+
+func getEventById(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not convert event id.", "error": err})
+		return
+	}
+
+	event, err := models.GetEventById(id)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not get event.", "error": err})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"event": event})
 }
 
 func getEvents(context *gin.Context) {
@@ -53,6 +74,7 @@ func createEvent(context *gin.Context) {
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save event.", "error": err})
+		return
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
