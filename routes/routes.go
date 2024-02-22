9c90c9c0fd43fa9353b99e1fe77a1c18ef23e8cc
Nathan Faria <nathanfaria607@gmail.com>
2024-02-22 09:39:31 -0300

    feat: add users table

--- routes/routes.go
+++ routes/routes.go
@@ -3,15 +3,14 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
+	//Events routes
 	server.GET("/", getHome)
-
 	server.GET("/events", getEvents)
-
 	server.GET("/events/:id", getEventById)
-
 	server.POST("/events", createEvent)
-
 	server.PUT("/events/:id", updateEvent)
-
 	server.DELETE("/events/:id", deleteEvent)
+
+	//User routes
+	server.POST("/signup")
 }
