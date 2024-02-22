7f00794962b4e82f1f617a821197f17e083e748b
Nathan Faria <nathanfaria607@gmail.com>
2024-02-22 14:56:13 -0300

    feat: add register event in registrations

--- models/event.go
+++ models/event.go
@@ -115,3 +115,19 @@ func (e Event) Delete() error {
 
 	return err
 }
+
+func (e Event) Register(userId int64) error {
+	query := "INSERT INTO registrations(event_id, user_id) VALUES($1, $2)"
+
+	prepareStatement, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer prepareStatement.Close()
+
+	_, err = prepareStatement.Exec(e.ID, userId)
+
+	return err
+}
--- routes/routes.go
+++ routes/routes.go
@@ -22,6 +22,8 @@ func RegisterRoutes(server *gin.Engine) {
 	authenticated.POST("/events", createEvent)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
+	authenticated.POST("/events/:id/register", registerForEvent)
+	authenticated.DELETE("/events/:id/register")
 
 	//User routes
 	server.POST("/signup", signup)
