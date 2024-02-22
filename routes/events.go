b5292918fbee78791bc9611230f767079c34e058
Nathan Faria <nathanfaria607@gmail.com>
2024-02-22 13:23:51 -0300

    feat: add validateToken function

--- routes/events.go
+++ routes/events.go
@@ -41,6 +41,15 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+
+	// Get token from header
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 
